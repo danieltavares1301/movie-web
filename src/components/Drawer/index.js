d23777f5951fd6135c15d18b884390b915f0a871
Daniel Tavares <daniel.tavares.1301@gmail.com>
2022-04-09 11:57:22 -0300

    style: estiliza Drawer e Header

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -4,10 +4,8 @@ import Drawer from "@mui/material/Drawer";
 import Toolbar from "@mui/material/Toolbar";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
+//import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 
 const drawerWidth = 240;
 
@@ -27,14 +25,21 @@ const PermanentDrawerLeft = () => (
     >
       <Toolbar />
       <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
+        <ListItem button key={1}>
+          {/*<ListItemIcon>
               {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+            </ListItemIcon>*/}
+          <ListItemText primary="Home" />
+        </ListItem>
+        <ListItem button key={2}>
+          <ListItemText primary="Movie" />
+        </ListItem>
+        <ListItem button key={3}>
+          <ListItemText primary="Session" />
+        </ListItem>
+        <ListItem button key={4}>
+          <ListItemText primary="User" />
+        </ListItem>
       </List>
     </Drawer>
   </Box>
