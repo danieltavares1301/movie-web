6fe68217b00ae5c7c0de2715645704a3e0b53910
Daniel Tavares <daniel.tavares.1301@gmail.com>
2022-04-09 19:14:23 -0300

    fix: corrige warning sobre React 18

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 import Router from "./Router";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./Theme";
 
-ReactDOM.render(
-    <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Router />
-    </ThemeProvider>, document.querySelector("#root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    React.S
+    <Router />
+  </ThemeProvider>
 );
