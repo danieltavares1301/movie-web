6a73f4e37a881114f4a0ff9521dbda09b767e5c0
Daniel Tavares <daniel.tavares.1301@gmail.com>
2022-04-10 15:24:07 -0300

    feat: botão para criar novo usuário

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,8 +1,23 @@
 import React from "react";
 import DenseTable from "../../components/Table";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
 
 const User = () => {
-  return <DenseTable />;
+  const navigate = useNavigate();
+
+  const onCreateUser = () => {
+    navigate("new");
+  };
+  return (
+    <>
+      <h3>Users</h3>
+      <Button variant="contained" onClick={() => onCreateUser()}>
+        Criar novo
+      </Button>
+      <DenseTable />
+    </>
+  );
 };
 
 export default User;
