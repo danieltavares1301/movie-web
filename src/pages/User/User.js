dbf83287ee101f08a0d5bd8e7756daf6e56de827
Daniel Tavares <daniel.tavares.1301@gmail.com>
2022-04-10 15:57:56 -0300

    perf: lista de países por meio de api

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,5 +1,36 @@
-import React from "react";
+import { Autocomplete, TextField } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const User = () => <div>user</div>;
+const User = () => {
+  const [countries, setCountries] = useState([]);
 
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      setCountries(
+        response.data.map((country) => ({
+          image: "",
+          label: country.name.common,
+        }))
+      );
+    });
+  }, []);
+
+  return (
+    <>
+      <div>user</div>
+      <TextField id="name" variant="outlined" label="name" />
+      <br />
+      <TextField id="email" variant="outlined" label="email" />
+      <br />
+      <Autocomplete
+        disablePortal
+        id="Country"
+        options={countries.map((item) => item.label)}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Country" />}
+      />
+    </>
+  );
+};
 export default User;
